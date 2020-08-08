ec1caa65dfa1d5c555fb8ea625d60caf247564af
VOKG <49445098+VOKG@users.noreply.github.com>
2020-08-08 19:23:03 +0300

    Initial commit

---
+++ lib/Data/DataTechnicalService.dart
@@ -0,0 +1,15 @@
+class DataTechnicalService {
+
+  String person;
+  String name;
+  num password;
+
+  num heater;
+  num oil;
+  num filter;
+  num spring;
+  String nameMachine;
+  num numberMachine;
+
+
+}
