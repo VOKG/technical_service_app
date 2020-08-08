1ef3a1ee7452986268edb347b4bb43f0ccbb5eb5
VOKG <49445098+VOKG@users.noreply.github.com>
2020-08-08 19:57:34 +0300

    create class ServiceData.dart

---
+++ lib/Model/ServiceData.dart
@@ -0,0 +1,10 @@
+class Service {
+  final String nameMachine;
+  final int numberMachine;
+  final String spares;// запчасти
+  final double quantity; // количество
+
+
+
+  Service(this.spares, this.quantity, this.nameMachine, this.numberMachine);
+}
